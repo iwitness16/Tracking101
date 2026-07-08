13d66b523ccd79ad151f2d9a1fc43cda8ef89a7b
bbrian23 <iwitness16@gmail.com>
2026-07-09 00:01:11 +0100

    All good

--- lib/site-contact.ts
+++ lib/site-contact.ts
@@ -1,6 +1,6 @@
 export const SITE_EMAIL = 'info@atlasswiftlogistics.com'
-export const WHATSAPP_E164 = '18259293315'
-export const PHONE_DISPLAY = '+1 (825) 929-3315'
+export const WHATSAPP_E164 = '14424516622'
+export const PHONE_DISPLAY = '+1 (442) 451-6622'
 
 export const mailtoHref = `mailto:${SITE_EMAIL}`
 
